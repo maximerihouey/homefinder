b794e6664fefa08d25c5fbafea02c15fc10bf03a
Jaona Ramahaleo <jaona.ramahaleo@gmail.com>
2016-04-30 12:16:00 +0200

    Adding 230 bus stations :bus:

--- homefinder/homefinder.py
+++ homefinder/homefinder.py
@@ -1,6 +1,7 @@
 """This module aims at helping me choose my future home in an optimal way."""
 
 import json
+import xmltodict
 
 
 class Placemark(dict):
@@ -45,14 +46,23 @@ class Homefinder(object):
     def add_from_file(self, filename):
         """Add :class:`Placemark` from a file."""
 
-    def add_velobleu(self, filepath):
+    def import_velobleu_stations(self, filepath):
         """Add Vélobleu stations from a file."""
         with open(filepath, 'r') as inputfile:
             data = json.load(inputfile)
             for pm in data['docs']:
-                latitude, longitude = pm['geometry']['coordinates']
+                lat, lon = pm['geometry']['coordinates']
                 self.add(Placemark(type='velobleu_station',
-                                   coordinates=[latitude, longitude, 0.0]))
+                                   coordinates=[lat, lon, 0.0]))
+
+    def import_bus_stations(self, filepath):
+        """Add bus stations from a file."""
+        with open(filepath, 'r') as inputfile:
+            doc = xmltodict.parse(inputfile.read())
+        for pm in doc['kml']['Document']['Placemark']:
+            coords = [float(a) for a in pm['Point']['coordinates'].split(',')]
+            self.add(Placemark(type='bus_station', name=pm['name'],
+                               coordinates=coords))
 
     def get_by_type(self, typename):
         """Retrieve all placemarks of type :param:`typename`."""
@@ -62,13 +72,20 @@ class Homefinder(object):
         """Return number of placemarks of type :param:`typename`."""
         return len(self.get_by_type(typename))
 
+    def state(self):
+        """Give the state of the current :class:`Homefinder` instance."""
+        types = set([pm['type'] for pm in self.placemarks])
+        return {typename: self.number_of(typename) for typename in types}
+
     def display_map(self):
         """Display a map of the facilities."""
         # ToDo
+        pass
 
     def optimize(self):
         """Find the optimal home."""
         # ToDo
+        pass
 
     def __str__(self):
         """String representation."""
--- test_homefinder.py
+++ test_homefinder.py
@@ -2,25 +2,7 @@
 
 from homefinder import homefinder
 
-pm1 = homefinder.Placemark(type='bus_station', coordinates=[1.2, 2.0, 0.0])
-pm2 = homefinder.Placemark(type='apartment', coordinates=[1.3, 1.2, 2.0])
-pm3 = homefinder.Placemark(type='tramway_station', coordinates=[1.1, 1.2, 0.0])
-print(pm1)
-
 hf = homefinder.Homefinder()
-hf.add(pm1)
-hf.add(pm2)
-hf.add(pm3)
-
-print(hf.get_by_type('apartment'))
-print(hf.get_by_type('bus_station'))
-print(hf.get_by_type('tramway_station'))
-print(hf.get_by_type('velobleu_station'))
-
-hf.add_velobleu("./data/velobleu.json")
-
-print(hf.get_by_type('velobleu_station'))
-print(hf.number_of('velobleu_station'))
-print(hf.number_of('apartment'))
-print(hf.number_of('bus_station'))
-print(hf.number_of('tramway_station'))
+hf.import_velobleu_stations("./data/velobleu.json")
+hf.import_bus_stations("./data/230.kml")
+print(hf.state())
