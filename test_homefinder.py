327dc732af95cfa1a26cce7cdc70b3741db73527
Jaona Ramahaleo <jaona.ramahaleo@gmail.com>
2016-05-02 23:34:03 +0200

    Add import for apartments + ID number to bus stations

--- test_homefinder.py
+++
@@ -1,8 +0,0 @@
-"""Test module for :class:`Homefinder`."""
-
-from homefinder import homefinder
-
-hf = homefinder.Homefinder()
-hf.import_velobleu_stations("./data/velobleu.json")
-hf.import_bus_stations("./data/230.kml")
-print(hf.state())
