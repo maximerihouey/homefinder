3f2fe6ace022a5c5f7883dc42965b7d8d9172fee
Jaona Ramahaleo <jaona.ramahaleo@gmail.com>
2016-05-02 23:34:03 +0200

    Add import for apartments + ID number to bus stations

--- homefinder/homefinder.py
+++ homefinder/homefinder.py
@@ -1,5 +1,7 @@
 """This module aims at helping me choose my future home in an optimal way."""
 
+import os
+import googlemaps
 import json
 import xmltodict
 
@@ -54,6 +56,9 @@ class Homefinder(object):
     def __init__(self):
         """Constructor."""
         self.placemarks = []
+        self.distance_matrix = []
+        self.key = os.environ.get('HOMEFINDER_GOOGLE_API_TOKEN')
+        self.gmaps = googlemaps.Client(self.key)
 
     def add(self, placemark):
         """Add a :class:`Placemark` to the :class:`Homefinder`."""
@@ -62,6 +67,21 @@ class Homefinder(object):
     def add_from_file(self, filename):
         """Add :class:`Placemark` from a file."""
 
+    def import_apartments(self, filepath):
+        """Add apartments from a file."""
+        with open(filepath, 'r') as inputfile:
+            data = json.load(inputfile)
+            for pm in data:
+                self.add(Placemark(type='apartment',
+                                   id=pm.id,
+                                   coordinates=[pm.latitude, pm.longitude, 0.],
+                                   rent=pm.rent,
+                                   surface=pm.surface,
+                                   extra_costs=pm.extra_costs,
+                                   info=pm.info,
+                                   ref=pm.ref,
+                                   source=pm.url))
+
     def import_velobleu_stations(self, filepath):
         """Add Vélobleu stations from a file."""
         with open(filepath, 'r') as inputfile:
@@ -69,6 +89,7 @@ class Homefinder(object):
             for pm in data['docs']:
                 lat, lon = pm['geometry']['coordinates']
                 self.add(Placemark(type='velobleu_station',
+                                   id=int(pm['IDENT']),
                                    coordinates=[lat, lon, 0.0]))
 
     def import_bus_stations(self, filepath):
@@ -78,7 +99,7 @@ class Homefinder(object):
         for pm in doc['kml']['Document']['Placemark']:
             coords = [float(a) for a in pm['Point']['coordinates'].split(',')]
             self.add(Placemark(type='bus_station', name=pm['name'],
-                               coordinates=coords))
+                               id=int(pm['id']), coordinates=coords))
 
     def get_by_type(self, typename):
         """Retrieve all placemarks of type :param:`typename`."""
@@ -92,6 +113,12 @@ class Homefinder(object):
         """Give the state of the current :class:`Homefinder` instance."""
         types = set([pm['type'] for pm in self.placemarks])
         return {typename: self.number_of(typename) for typename in types}
+
+    def create_distance_matrix(self):
+        """Distance matrix of the current :class:`Homefinder` instance."""
+        apartments = self.get_by_type('apartment')
+        bus_stations = self.get_by_type('bus_station')
+        return apartments, bus_stations
 
     def display_map(self):
         """Display a map of the facilities."""
--- test_homefinder.py
+++ test_homefinder.py
@@ -1,8 +1,20 @@
 """Test module for :class:`Homefinder`."""
 
 from homefinder import homefinder
+from pprint import pprint
 
 hf = homefinder.Homefinder()
 hf.import_velobleu_stations("./data/velobleu.json")
 hf.import_bus_stations("./data/230.kml")
-print(hf.state())
+# print(hf.get_state())
+
+routes = hf.gmaps.directions("15 rue Fornero Meneï, 06300 Nice",
+                             "Lycée Masséna, Nice",
+                             mode='walking',
+                             units='metrics')
+
+pprint(routes)
+
+durations = [route['legs'][0]['duration']['text'] for route in routes]
+
+pprint(durations)
